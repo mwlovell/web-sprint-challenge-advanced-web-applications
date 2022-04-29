2a979134823d7d096de84436a69c9d687a80bac1
Mike Lovell <mike@Carlys-MacBook-Air.local>
2022-04-29 07:36:04 -0700

    worked on login, should be done?, 1 test passing

--- frontend/components/LoginForm.js
+++ frontend/components/LoginForm.js
@@ -22,10 +22,12 @@ export default function LoginForm(props) {
   }
 
   const isDisabled = () => {
-    // ✨ implement
-    // Trimmed username must be >= 3, and
-    // trimmed password must be >= 8 for
-    // the button to become enabled
+    if (values.username.trim().length >= 3&& values.password.trim().length >= 8 ) {
+      return false;
+    }else {
+      return true;
+    }
+   
   }
 
   return (
