7af3a135cd8cf185ac05cfc4381a7ec64f24ffb1
Mike Lovell <mike@Carlys-MacBook-Air.local>
2022-04-29 13:07:32 -0700

    built get an dpost artricles in app.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -8,27 +8,29 @@ import Spinner from './Spinner'
 import axios from 'axios';
 import axiosWithAuth from '../axios/index';
 
-const articlesUrl = 'http://localhost:9000/api/articles'
-const loginUrl = 'http://localhost:9000/api/login'
+const articlesUrl = 'http://localhost:9000/api/articles';
+const loginUrl = 'http://localhost:9000/api/login';
 
 export default function App() {
   // ✨ MVP can be achieved with these states
-  const [message, setMessage] = useState('')
-  const [articles, setArticles] = useState([])
-  const [currentArticleId, setCurrentArticleId] = useState()
-  const [spinnerOn, setSpinnerOn] = useState(false)
+  const [message, setMessage] = useState('');
+  const [articles, setArticles] = useState([]);
+  const [currentArticleId, setCurrentArticleId] = useState();
+  const [spinnerOn, setSpinnerOn] = useState(false);
 
   // ✨ Research `useNavigate` in React Router v.6
-  const navigate = useNavigate()
-  const redirectToLogin = () => { /* ✨ implement */ }
-  const redirectToArticles = () => { /* ✨ implement */ }
+  const navigate = useNavigate();
+  const redirectToLogin = () => { 
+      navigate('/');
+     };
+  const redirectToArticles = () => { 
+    navigate('/articles');
+   }
 
   const logout = () => {
-    // ✨ implement
-    // If a token is in local storage it should be removed,
-    // and a message saying "Goodbye!" should be set in its proper state.
-    // In any case, we should redirect the browser back to the login screen,
-    // using the helper above.
+    window.localStorage.removeItem('token');
+        setMessage('Goodbye!');
+        redirectToLogin();
   }
 
   const login = ({ username, password }) => {
@@ -47,30 +49,41 @@ export default function App() {
           .finally(() => {
               setSpinnerOn(false);
           });
-    // ✨ implement
-    // We should flush the message state, turn on the spinner
-    // and launch a request to the proper endpoint.
-    // On success, we should set the token to local storage in a 'token' key,
-    // put the server success message in its proper state, and redirect
-    // to the Articles screen. Don't forget to turn off the spinner!
-  }
+     
+  };
 
   const getArticles = () => {
-    // ✨ implement
-    // We should flush the message state, turn on the spinner
-    // and launch an authenticated request to the proper endpoint.
-    // On success, we should set the articles in their proper state and
-    // put the server success message in its proper state.
-    // If something goes wrong, check the status of the response:
-    // if it's a 401 the token might have gone bad, and we should redirect to login.
-    // Don't forget to turn off the spinner!
+    setMessage('');
+        setSpinnerOn(true);
+        axiosWithAuth()
+            .get(articlesUrl)
+            .then((res) => {
+                setArticles(res.data.articles);
+                setMessage(res.data.message);
+            })
+            .catch((err) => {
+                if (err.resp.status === 401) {
+                    redirectToLogin();
+                } else {
+                    setMessage(err.res.data.message);
+                }
+            })
+            .finally(() => {
+                setSpinnerOn(false);
+            });
   }
 
   const postArticle = article => {
-    // ✨ implement
-    // The flow is very similar to the `getArticles` function.
-    // You'll know what to do! Use log statements or breakpoints
-    // to inspect the response from the server.
+    axiosWithAuth()
+            .post(articlesUrl, article)
+            .then((res) => {
+                setArticles([...articles, res.data.article]);
+                setMessage(res.data.message);
+            })
+            .catch((err) => {
+                setMessage(err.response.data.message);
+            });
+    
   }
 
   const updateArticle = ({ article_id, article }) => {
