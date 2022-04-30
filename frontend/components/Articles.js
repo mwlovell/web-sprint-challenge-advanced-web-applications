d4481bbe64744799c9393e11e586b9342c39102b
Mike Lovell <mike@Carlys-MacBook-Air.local>
2022-04-29 18:11:01 -0700

    built articles, 2 passing need jsx in app anbd tests fo rspinner

--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -3,14 +3,15 @@ import { Navigate } from 'react-router-dom'
 import PT from 'prop-types'
 
 export default function Articles(props) {
-  // ✨ where are my props? Destructure them here
+  const { articles, getArticles, deleteArticle, setCurrentArticleId } = props;
 
-  // ✨ implement conditional logic: if no token exists
-  // we should render a Navigate to login screen (React Router v.6)
+  if (!window.localStorage.getItem('token')) {
+      return <Navigate to='/' />;
+  }
 
   useEffect(() => {
-    // ✨ grab the articles here, on first render only
-  })
+      getArticles();
+  }, []);
 
   return (
     // ✨ fix the JSX: replace `Function.prototype` with actual functions
@@ -20,7 +21,7 @@ export default function Articles(props) {
       {
         ![].length
           ? 'No articles yet'
-          : [].map(art => {
+          : articles.map(art => {
             return (
               <div className="article" key={art.article_id}>
                 <div>
@@ -29,8 +30,18 @@ export default function Articles(props) {
                   <p>Topic: {art.topic}</p>
                 </div>
                 <div>
-                  <button disabled={true} onClick={Function.prototype}>Edit</button>
-                  <button disabled={true} onClick={Function.prototype}>Delete</button>
+                <button
+                    disabled={false}
+                      onClick={() => setCurrentArticleId(art.article_id)}
+                >
+                  Edit
+                </button>
+                <button
+                    disabled={false}
+                      onClick={() => deleteArticle(art.article_id)}
+                >
+                  Delete
+                </button>
                 </div>
               </div>
             )
